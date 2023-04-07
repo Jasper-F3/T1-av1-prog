ea3b62f8728bf3c83604c7e4281eb038dc0c51e5
Victor Emanuel Mello <victoremanuel945@gmail.com>
2023-04-06 23:19:42 -0300

    first commit

---
+++ av01-t1/src/main.js
@@ -0,0 +1,1 @@
+import * as te from "tw-elements";
---
+++ av01-t1/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      borderWidth: {
+        1: "1px",
+      },
+    },
+  },
+  plugins: [],
+};
