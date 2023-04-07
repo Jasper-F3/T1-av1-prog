6eea4c40692054885e753d8fb63abdc3625e0537
Rafael Vargas <rafael.vargas.emp@gmail.com>
2023-04-07 15:04:25 -0300

    home screen ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       borderWidth: {
         1: "1px",
       },
+      colors: {
+        primary: "#2D2727",
+        secundary: "#222222",
+        html: "#FF6000",
+        css: "#2F58CD",
+      },
     },
   },
   plugins: [],
